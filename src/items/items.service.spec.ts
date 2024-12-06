9a0c8204847dad106749eff96cb39f01f79de79c
Haroon Abdulrazaq <haroonabdulrazaq@gmail.com>
2024-12-06 18:40:24 +0100

    Add test

--- src/items/items.service.spec.ts
+++ src/items/items.service.spec.ts
@@ -1,18 +1,44 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ItemsService } from './items.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Item } from './entities/item.entity';
+import { EntityManager, Repository } from 'typeorm';
+import { Listing } from './entities/listing.entity';
 
 describe('ItemsService', () => {
   let service: ItemsService;
+  let itemRepository: Repository<Item>
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ItemsService],
+      providers: [ItemsService, {
+        provide: getRepositoryToken(Item),
+        useValue: {
+          find: jest.fn()
+        }
+      },
+      {
+        provide: EntityManager,
+        useValue: {}
+      },
+     {
+      provide: getRepositoryToken(Listing),
+      useValue: {}
+    }
+    ],
     }).compile();
 
     service = module.get<ItemsService>(ItemsService);
+    itemRepository= module.get<Repository<Item>>(getRepositoryToken(Item))
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should return all items', async ()=>{
+   await service.findAll();
+
+   expect(itemRepository.find).toHaveBeenCalled();
+  })
 });
